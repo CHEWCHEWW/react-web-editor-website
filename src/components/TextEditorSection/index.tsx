88974c47a249f1f4b4647b3b884dd7360e7d4ae9
Minjieun <chewchewchew3@gmail.com>
2021-08-12 11:37:20 +0900

    [ADD]: Separate text area

--- src/components/TextEditorSection/index.tsx
+++ src/components/TextEditorSection/index.tsx
@@ -1,9 +1,41 @@
+import dynamic from "next/dynamic";
 import React from "react";
+import { TextEditorBlock } from "react-web-editor";
+import styled from "styled-components";
+
+import { TEXT_EDITOR_BLOCK } from "../../constants/codeExample";
+import { DEFAULT_LEFT, DEFAULT_TOP } from "../../constants/ui";
+import EditorCard from "../EditorCard";
+
+const CodeEditor = dynamic(
+  () => import("../../components/CodeEditor"),
+  { ssr: false }
+);
 
 const TextEditorSection: React.FC = (): React.ReactElement => {
   return (
-    <div></div>
+    <EditorSection>
+      <CodeEditor code={TEXT_EDITOR_BLOCK} />
+      <EditorCard width={40} height={30} backgroundColor={"white"} unit={"rem"}>
+        <TextEditorBlock
+          width={20}
+          height={20}
+          top={0}
+          left={0}
+          unit={"rem"}
+          parentStyle={{ width: 40, height: 40, top: DEFAULT_TOP, left: DEFAULT_LEFT }}
+        />
+      </EditorCard>
+    </EditorSection>
   );
 };
+
+const EditorSection = styled.section`
+  display: flex;
+  width: 20rem;
+  height: 15rem;
+  background-color: orange;
+
+`;
 
 export default TextEditorSection;
--- src/constants/codeExample.ts
+++ src/constants/codeExample.ts
@@ -1,14 +1,28 @@
-export const STYLE_EDITOR_BLOCK = `import { SyledEditorBlock } from "react-web-editor"
+export const STYLE_EDITOR_BLOCK = `import { StyledEditorBlock } from "react-web-editor"
   ...
   return (
-    <StyleEditorBlock 
-      width={300} 
-      height={100} 
-      top={500} 
-      left={800}
-      parentStyle={{width: 500, height: 300, left: 200, top: 200}}
+    <StyleEditorBlock
+      width={300}
+      height={100}
+      top={20}
+      left={30}
+      parentStyle={{ width: 500, height: 300, left: 0.1, top: 0.1 }}
     >
-      <TestDiv>happy</TestDiv>
+      <Circle color={"#F2BFB5"} />
     </StyleEditorBlock>
   );
 `;
+
+export const TEXT_EDITOR_BLOCK = `import { TextEditorBlock } from "react-web-editor"
+  ...
+  return (
+    <TextEditorBlock
+      width={300}
+      height={100}
+      top={500}
+      left={800}
+      parentStyle={{ width: 500, height: 300, left: 200, top: 200 }}
+    />
+  );
+`;
+
