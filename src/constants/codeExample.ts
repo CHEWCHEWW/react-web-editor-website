92daac369089328545e1fa279fa63d0a9319b5db
Minjieun <chewchewchew3@gmail.com>
2021-08-14 00:24:33 +0900

    [ADD]: Hook Section layout

--- src/constants/codeExample.ts
+++ src/constants/codeExample.ts
@@ -6,6 +6,7 @@ export const STYLE_EDITOR_BLOCK = `import { StyledEditorBlock } from "react-web-
       height={100}
       top={20}
       left={30}
+      unit={"rem"}
       parentStyle={{ width: 500, height: 300 }}
     >
       <Circle color={"#F2BFB5"} />
@@ -21,6 +22,7 @@ export const TEXT_EDITOR_BLOCK = `import { TextEditorBlock } from "react-web-edi
       height={100}
       top={500}
       left={800}
+      unit={"rem"}
       parentStyle={{ width: 500, height: 300 }}
     />
   );
@@ -38,3 +40,24 @@ export const DRAG_AND_DROP_BLOCK = `import { DragAndDropTable } from "react-web-
   );
 `;
 
+export const HOOK_USAGE_EXAMPLE = `import React, { useState } from "react";
+import { DraggableHandler, useDraggable } from "react-web-editor"
+  ...
+  const [componentStyle, setComponentStyle] = useState<ComponentStyle>({
+    width,
+    height,
+    top,
+    left,
+  });
+
+  const {
+    handleDragStart,
+    handleDragEnd,
+  } = useResize({
+    ...componentStyle,
+    unit,
+    onDrag: setComponentStyle,
+    dragBoardOption: parentStyle,
+  });
+  ...
+`;
